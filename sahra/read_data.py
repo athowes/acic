8cc112d9d00dc6bddf604dd5ed49a8266f4c38ea
Sahra Ghalebikesabi <s.ghalebikesabi@gmail.com>
2021-09-03 13:36:20 +0100

    superlearner

--- sahra/read_data.py
+++ sahra/read_data.py
@@ -47,6 +47,7 @@ for param_idx, param in enumerate([30, 40, 50]):
         results_arr[seed_idx, param_idx, 2] = reg.coef_[0][0]
 
         # %%
+        # estimate ate with g-formula
         x_cols = [f"t_{i}" for i in range(x.shape[1])]
         post_data_df = pd.DataFrame(
             np.concatenate((y[:, None], z[:, None], x), 1), columns=["y", "z"] + x_cols
@@ -66,7 +67,15 @@ for param_idx, param in enumerate([30, 40, 50]):
         # results_arr[seed_idx, param_idx, 2] = ((r_all - r_none) - r_untr * (1 - perc_treated)) / perc_treated
         results_arr[seed_idx, param_idx, 3] = r_all - r_none
 
+        # estimate ate with AIPTW
         sdr = AIPTW(post_data_df, exposure="z", outcome="y")
+        sdr.exposure_model(" + ".join(x_cols))
+        sdr.outcome_model("z + " + " + ".join(x_cols))
+        sdr.fit()
+        results_arr[seed_idx, param_idx, 4] = sdr.average_treatment_effect
+
+        # estimate ate with IPW
+        sdr = IPTW(post_data_df, exposure="z", outcome="y")
         sdr.exposure_model(" + ".join(x_cols))
         sdr.outcome_model("z + " + " + ".join(x_cols))
         sdr.fit()
@@ -76,13 +85,43 @@ for param_idx, param in enumerate([30, 40, 50]):
 # estimate atet with regression with covariates
 import zepid as ze
 from zepid.causal.gformula import TimeFixedGFormula
-
+from zepid.causal.doublyrobust import AIPTW
+from zepid.causal.ipw import IPTW
+import supylearner as sl
 
 from zepid import load_sample_data, spline, RiskDifference
 from zepid.causal.gformula import TimeFixedGFormula, SurvivalGFormula
 from zepid.causal.ipw import IPTW, IPMW
 from zepid.causal.snm import GEstimationSNM
-from zepid.causal.doublyrobust import AIPTW, TMLE
+
+from zepid.causal.doublyrobust import TMLE
+
+
+def SuPyFitter(X, y):
+    svm = SVC(kernel="linear", probability=True, random_state=101)
+    log1 = LogisticRegression(penalty="l1", random_state=201)
+    log2 = LogisticRegression(penalty="l2", random_state=103)
+    randf = RandomForestClassifier(random_state=141)
+    adaboost = AdaBoostClassifier(random_state=505)
+    bayes = GaussianNB()
+    lib = [svm, log1, log2, randf, adaboost, bayes]
+    libnames = ["SVM", "Log_L1", "Log_L2", "Random Forest", "AdaBoost", "Bayes"]
+    sl = supylearner.SuperLearner(lib, libnames, loss="nloglik", K=10)
+    sl.fit(X, y)
+    sl.summarize()
+    return sl
+
+
+import numpy as np
+import supylearner
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import (
+    RandomForestClassifier,
+    AdaBoostClassifier,
+)  # Random Forest, AdaBoost
+from sklearn.naive_bayes import GaussianNB
+
 
 x_cols = [f"t_{i}" for i in range(x.shape[1])]
 post_data_df = pd.DataFrame(
